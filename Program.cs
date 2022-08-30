43b67ee54ba59c8caf060ff405384fb07e4ee7ab
unknown <gritsunov85@gmail.com>
2022-08-30 10:38:54 +0600

    end

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите b1: "); 
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите b2: "); 
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k1: "); 
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k2: "); 
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = 0;
+
+double y = 0;
+
+if ((k1 == k2) || (b1 == b2)){
+    Console.WriteLine("Прямые совпадают");    
+}
+else{
+    x = (b2 - b1) / (k1 - k2);
+
+    y=(k1*(b2-b1))/(k1-k2)+b1;
+
+    Console.WriteLine(Convert.ToString(x) + "; " + Convert.ToString(y));
+
+}
+
